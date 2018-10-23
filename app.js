ccef215f47ba861e66c8cb73bf79a526c9c6a98c
Bastian Hartenstein <hartenstein.bastian@gmail.com>
2018-10-23 13:46:31 +0200

    Anfang Detailanzeige Javascript muss anders begonnen werden

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+Grundsystem der Anwendung
+*/
+
+class App {
+
+    constructor () {
+        this._daten = [
+            {
+                vorname: "",
+                nachname: "",
+                geburtsdatum: "",
+                tel: "",
+                adresse: "",
+                satz: ""
+            },
+            {
+                vorname: "Albert",
+                nachname: "Einstein",
+                geburtsdatum: "01.01.2000",
+                tel: "123456",
+                adresse: "lulweg 1",
+                satz: "I bims, albert vong einstein her"
+            }
+        ];
+    }
+
+}
