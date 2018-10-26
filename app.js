9c45fc4fc713b6227747dcd9879ece17789796b9
chridue95 <christopherduerr@gmx.de>
2018-10-26 18:07:51 +0200

    nix

--- app.js
+++ app.js
@@ -7,27 +7,15 @@ Grundsystem der Anwendung
 class App {
 
     constructor () {
-        this._daten = [
-            {
-                vorname: "",
-                nachname: "",
-                geburtsdatum: "",
-                tel: "",
-                adresse: "",
-                satz: ""
-            },
-            {
+        this._daten =
+            [
                 vorname: "Albert",
                 nachname: "Einstein",
                 geburtsdatum: "01.01.2000",
                 tel: "123456",
                 adresse: "lulweg 1",
                 satz: "I bims, albert vong einstein her"
-            }
-        ];
+            ]
     }
-    function myFunction() {
-      var element = document.getElementById("visitenkartePLUS");
-      element.classList.add("mystyle");
-    }
+
 }
