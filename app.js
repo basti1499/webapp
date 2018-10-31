616f04897f5a3faf1458cadf6c9db4917aa7f17d
Bastian Hartenstein <hartenstein.bastian@gmail.com>
2018-10-31 10:39:17 +0100

    CSS: +rote Farbe für Löschen Button +Cursor: Pointer bei Logo JS: *aufgeräumt

--- app.js
+++ app.js
@@ -137,7 +137,6 @@ function freundAnzeigen(id) {
     document.getElementById("detailEmail").innerHTML = _daten[id].email;
     document.getElementById("detailAdresse").innerHTML = _daten[id].address;
     document.getElementById("detailTel").innerHTML = _daten[id].phone;
-    //document.getElementById("detailanzeige").innerHTML = document.getElementById("detailanzeige").innerHTML.replace("$ID$", id); // Fehler, löschen
     document.getElementById("delButton").setAttribute("onclick", "freundEntfernen(" + id + ")");
 
     showDetails();
